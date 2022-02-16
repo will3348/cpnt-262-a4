ee5f888b669099daa9b710b490c8d05b9d9d6424
Will Tengyuan Li <will19940430@gmail.com>
2022-02-15 22:51:16 -0700

    added html and js files

---
+++ index.js
@@ -0,0 +1,25 @@
+const form = document.querySelector("form");
+
+const submitHandler = (event) => {
+  event.preventDefault();
+
+  const color = document.getElementById("dropdown");
+
+  if (color.value === "blue") {
+    form.style.backgroundColor = "blue";
+  } 
+  if (color.value === "pink") {
+    form.style.backgroundColor = "pink";
+  }
+  if (color.value === "yellow") {
+    form.style.backgroundColor = "yellow";
+  }
+  if (color.value === "green") {
+    form.style.backgroundColor = "green";
+  }
+  if (color.value === "red") {
+    form.style.backgroundColor = "red";
+  } 
+};
+
+form.addEventListener("submit", submitHandler);
